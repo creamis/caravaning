e9a84f7e94c1c949db81708baf210013ef980bbb
creamis <85139993+creamis@users.noreply.github.com>
2026-09-13 22:57:12 +0200

    Corregir rutas de imágenes generadas CamperDays Valencia

---
+++ blog/migrations/0083_corregir_imagenes_generadas_valencia.py
@@ -0,0 +1,41 @@
+from django.db import migrations
+
+POST_SLUG = "alquiler-camper-valencia"
+
+# Las imágenes generadas para CamperDays Valencia se sirven desde /static/.
+# PythonAnywhere ya sirve /static/ correctamente, por lo que evitamos depender
+# de la configuración de /media/ para estas imágenes editoriales.
+STATIC_IMAGES = [
+    "/static/images/blog/camperdays/valencia/valencia_hero.webp",
+    "/static/images/blog/camperdays/valencia/valencia_playa.webp",
+    "/static/images/blog/camperdays/valencia/valencia_albufera.webp",
+    "/static/images/blog/camperdays/valencia/valencia_ruta.webp",
+]
+
+
+def replace_images(apps, schema_editor):
+    Post = apps.get_model("blog", "Post")
+    PostImage = apps.get_model("blog", "PostImage")
+
+    post = Post.objects.filter(slug=POST_SLUG).first()
+    if not post:
+        return
+
+    PostImage.objects.filter(post=post).delete()
+
+    for url in STATIC_IMAGES:
+        PostImage.objects.create(
+            post=post,
+            image="",
+            image_url=url,
+        )
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("blog", "0082_imagenes_generadas_alquiler_camper_valencia"),
+    ]
+
+    operations = [
+        migrations.RunPython(replace_images, migrations.RunPython.noop),
+    ]
